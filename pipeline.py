39763082e4d375cfa94463c6a9873b9dc4b2fb2b
Merve Noyan <merve@huggingface.co>
2021-12-08 11:39:54 +0000

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -24,8 +24,7 @@ class PreTrainedPipeline():
 
 
 
-    def load_image(image_path):
-        img = tf.io.read_file(image_path)
+    def load_image(img):
         img = tf.io.decode_jpeg(img, channels=3)
         img = tf.image.resize(img, (299, 299))
         img = tf.keras.applications.inception_v3.preprocess_input(img)
